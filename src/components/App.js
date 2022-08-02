e12c1b72cf9494249d6088f713515c97ff70d44d
Valerie K <valerik210695@gmail.com>
2022-08-02 17:46:06 +0300

    header markup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="header__container">
+          <h1 className='header__title'>Pic Finder</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
